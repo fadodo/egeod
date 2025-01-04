e3723ca008aaecab45ff78f30c715f0490296357
fadodo <fadodo@users.noreply.github.com>
2025-01-04 10:47:22 +0100

    updat2

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
-import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
